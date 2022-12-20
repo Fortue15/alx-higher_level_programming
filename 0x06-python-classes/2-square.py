3f46dbfb757a7b9f72c829c08f4e918d708db6ed
Fortue15 <fortuechigogo3@gmail.com>
2022-12-20 19:52:06 +0000

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,12 +19,4 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
 
-        self.__size = size
-
-    def area(self):
-        """
-        Calculate area of the square
-        Returns: The square of the size
-        """
-
-        return (self.__size ** 2)
+        self.__size = 
