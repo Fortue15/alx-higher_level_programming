83cb66eab0b55644a3ab753704f5a4d31bc71164
Fortue15 <fortuechigogo3@gmail.com>
2023-01-06 01:02:43 +0000

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -32,6 +32,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
       @property
     def height(self):
         """retrieves height attribute"""
