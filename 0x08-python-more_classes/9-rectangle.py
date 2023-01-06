c4eebc3c02f05cf5f03b439eab3904456c099fe1
Fortue15 <fortuechigogo3@gmail.com>
2023-01-06 01:07:20 +0000

    0-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """this represents a rectangle"""
     number_of_instances = 0
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
         Args:
