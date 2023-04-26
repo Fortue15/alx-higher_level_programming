f0be4e15c52288f3de8af2fca01c1c2d6cd0646c
Fortue15 <Fortuechigogo3@gmail.com>
2023-04-26 22:59:39 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-const argms = process.argv[2];
-
-fs.readFile(argms, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data.toString());
-  }
-});
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString('utf-8'));
+    }
+  });
+}
